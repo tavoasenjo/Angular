c654b4fdc2a7d33d91d594b350dfe0670be8306d
Gustavo Asenjo <gasenjo@forgemedia.ca>
2016-01-12 15:40:50 -0500

    jan 12

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,3 @@
 // the first parameter is the name of the application! Te second parameter is an empty array, and defines the dependencies of the app. If you dont have dependencies, just leave an empty array
-angular.module("whishList", []);
+angular.module("whishList", [])
+.controller('mainCtrl');
