dc135da4fb94ee9b0a6639b32914b80b525705e5
Tavo <tavoasenjo@gmail.com>
2016-01-17 11:04:57 -0500

    Jan 17

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
 // the first parameter is the name of the application! Te second parameter is an empty array, and defines the dependencies of the app. If you dont have dependencies, just leave an empty array
 angular.module("whishList", [])
 .controller('mainCtrl', function($scope, dataService){
+	$scope.addWishes = function(){
+		var wish = {name: "This is a new wish."};
+		$scope.wishes.push(wish); 
+	};
+
 	$scope.helloConsole = dataService.helloConsole;
 
 	$scope.learningNgChange = function(){
